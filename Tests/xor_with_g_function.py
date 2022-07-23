f8bf5a9af280cad7a790e84f60548ce22211cf90
Horacio Gomez <gomezacevedohoracio@uams.edu>
2022-07-23 07:53:25 -0500

    adding one extra file

---
+++ Tests/xor_with_g_function.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Make data.
+X = np.arange(-0.15, 1.15, 0.05)
+Y = np.arange(-0.15, 1.15, 0.05)
+X, Y = np.meshgrid(X, Y)
+Z = 4*X*Y-2*X-2*Y+1
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
